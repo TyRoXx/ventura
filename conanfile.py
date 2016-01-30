370a1fb96c79c23bab2cae0bcd32ccb0fb5fa58f
TyRoXx <tyroxxxx@gmail.com>
2016-01-30 13:49:07 +0100

    make this library a conan.io package

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile
+import os
+
+class VenturaConan(ConanFile):
+    name = "ventura"
+    version = "0.1"
+    generators = "cmake"
+    requires = "silicium/0.2@TyRoXx/master"
+    url="http://github.com/tyroxx/ventura"
+    license="MIT"
+    exports="ventura/*"
+
+    def package(self):
+        self.copy(pattern="*.hpp", dst="include/ventura", src="ventura", keep_path=True)
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
